4cfd39249fbd855a489f34fbe7105208a39f93d0
Shauli Ravfogel <shauli321@gmail.com>
2019-06-30 12:30:26 +0300

    added closest-neighbor test
    evaluates the number of closest-vectors pairs that share the same dependency edge)

--- analysis/evaluator.py
+++ analysis/evaluator.py
@@ -1,16 +1,23 @@
 from syntactic_extractor import SyntacticExtractor
+from allennlp.commands.elmo import ElmoEmbedder
 from typing import List
 import copy
 import numpy as np
+import random
+random.seed(0)
+from collections import Counter
+import spacy
+import tqdm
 
 class Vector(object):
     
-    def __init__(self, vec, sentence, index):
+    def __init__(self, vec, sentence, index, dep):
         
         self.vec = vec
         self.sentence = sentence
         self.index = index
         self.size = np.linalg.norm(self.vec)
+        self.dep = dep
     
     def get_word(self):
         
@@ -54,117 +61,173 @@ class Vector(object):
         closest = max(vecs, key = lambda vector: vector.similarity(vec))
         return closest
         
+
+
+
+
+
+
+
+
         
 class Evaluator(object):
 
-	def __init__(self, extractor: SyntacticExtractor):
-	
-		self.extractor = extractor
-		self.sentences = self._load_sents()
-		self.parsed_sentences = self._parse(self.sentences)
-		self.elmo = self._load_elmo()
-		
-	def _load_elmo(self):
-	
-		options_file = "../data/external/elmo_2x4096_512_2048cnn_2xhighway_options.json"
-		weight_file = "../data/external/elmo_2x4096_512_2048cnn_2xhighway_weights.hdf5"
-		return FastElmo(options_file, weight_file)
-	
-	def _run_elmo(sentences: List[List[str]]) -> List[np.ndarray]:
-	
-	
-	        elmo_embeddings = self.elmo.embed_sentences(sentences)
-	        all_embeddings = []
-	        
-	        for sent_emn in elmo_embeddings:
-	        
-	                last_layer = sent_emn[:, -1]
-	                all_embeddings.append(last_layer)
-	        
-	        return all_embeddings
-	        
-		
-	def _load_sents(self, fname = "sents_f", max_length = 25) -> List[List[str]]:
-	
-		with open(fname, "r") as f:
-			lines = f.readlines()
-			lines =  [line.strip().split(" ") for line in lines]
-			
-		if max_length is not None:
-			lines = list(filter(lambda sentence: len(sentence) < max_length, lines))
-		return lines
-		
-	def _parse(self, sentences: List[List[str]]) -> List[List[str]]:
-	                                             
-		tokens_dict = {" ".join(sentence):sentence for sentence in sentences}
-		def custom_tokenizer(text): return tokens_dict[text]
-		nlp = spacy.load('en_core_web_sm')
-		#parser = nlp.create_pipe("parser")
-		all_deps = []
-		count = 0
-		 
-		for sent in sentences:
-		
-			doc = spacy.tokens.Doc(vocab=nlp.vocab, words=sent)
-			for name, proc in nlp.pipeline:
-				doc = proc(doc)
-				
-			deps = [token.dep_ for token in doc]
-			all_deps.append(deps)
-			
-			assert len(deps) == len(sent)
-		
-		return all_deps
+        def __init__(self, extractor: SyntacticExtractor):
+        
+                self.extractor = extractor
+                self.sentences = self._load_sents()
+                self.parsed_sentences = self._parse(self.sentences)
+                self.elmo = self._load_elmo()
+                self.elmo_embeddings = self._run_elmo()
+                self.vec_lists = self._list_vectors(self.elmo_embeddings, self.sentences, self.parsed_sentences)
+        
+        
+        def test(self):
+        
+                print("cloest neighbor test, before the application of the syntactic extractor")
+                self.closest_vector_test()
+                
+        def closest_vector_test(self, n = 1000):
+        
+                vec_lists = self.vec_lists
+                all_vecs = [v for sent_vecs in vec_lists for v in sent_vecs] # all vecs in a single list.
+                random.shuffle(all_vecs)
+                
+                good, bad = 0., 0.
+                
+                for i in range(n):
+                
+                        vec = all_vecs[i]
+                        nearest_neighbor = Vector.get_closest_vector(vec, vecs)
+                        
+                        if vec.dep == nearest_neighbor.dep:
+                        
+                                good += 1
+                        
+                        else:
+                                bad += 1
+                        
+                acc = good / (good + bad)
+                print(acc)
+                
+        def _load_elmo(self):
+        
+                print("Loading ELMO...")
+                
+                options_file = "../data/external/elmo_2x4096_512_2048cnn_2xhighway_options.json"
+                weight_file = "../data/external/elmo_2x4096_512_2048cnn_2xhighway_weights.hdf5"
+                return ElmoEmbedder(options_file, weight_file, cuda_device=cuda_device)
+        
+        def _run_elmo(sentences: List[List[str]]) -> List[np.ndarray]:
+        
+                print("Running ELMO...")
+                
+                elmo_embeddings = []# self.elmo.embed_sentences(sentences)
+                for sent in tqdm.tqdm(sentences):
+                
+                        elmo_embeddings.append(self.elmo.embed_sentence(sent))
+                        
+                all_embeddings = []
+                
+                for sent_emn in elmo_embeddings:
+                
+                        last_layer = sent_emn[:, -1]
+                        all_embeddings.append(last_layer)
+                
+                return all_embeddings
+                
+                
+        def _load_sents(self, fname = "sents_f", max_length = 25) -> List[List[str]]:
+        
+                print("Loading sentences...")
+                
+                with open(fname, "r") as f:
+                        lines = f.readlines()
+                        lines =  [line.strip().split(" ") for line in lines]
+                        
+                if max_length is not None:
+                        lines = list(filter(lambda sentence: len(sentence) < max_length, lines))
+                return lines
+                
+        def _parse(self, sentences: List[List[str]]) -> List[List[str]]:
+                             
+                print("Parsing...")
+                                        
+                tokens_dict = {" ".join(sentence):sentence for sentence in sentences}
+                def custom_tokenizer(text): return tokens_dict[text]
+                nlp = spacy.load('en_core_web_sm')
+                #parser = nlp.create_pipe("parser")
+                all_deps = []
+                count = 0
+                 
+                for sent in tqdm.tqdm(sentences):
+                
+                        doc = spacy.tokens.Doc(vocab=nlp.vocab, words=sent)
+                        for name, proc in nlp.pipeline:
+                                doc = proc(doc)
+                                
+                        deps = [token.dep_ for token in doc]
+                        all_deps.append(deps)
+                        
+                        assert len(deps) == len(sent)
+                
+                return all_deps
 
-	def _list_vectors(self, sents_embeddings: List[np.ndarray], sents: List[List[str]]) -> List[List[Vector]]:
-	
-	
-		"""
-		Transform the list of all state vectors (as numpy arrays) to a list of Vector objects.
-		
-		Parameters
-		
-		---------
-		sents_embeddings: ``List[np.ndarray]``, required
-		
-		        A list of ELMO embeddings for all sentences. Each list element is ELMO embedding 
-		        of a different sentence, with dimensions (sentence_length, 1024)
-		
-		sents: `` List[List[str]]``, required
-		
-		        A list of all sentences. Each list contains a list representing a different sentence.
-		        
-		Returns
-		---------
-		
-		all_vectors: ``List[List[Vector]``
-		       
-		       A list of lists of Vector objects. all_vectors[i][j] is the representation of the jth word
-		       in the ith sentence.
-		        
-		"""
-		
-		print("Creating Vector objects required for nearest neighbor search...")
-		
-		assert len(sents) == len(sents_embeddings)
-		
-		num_sentences = len(sents)
-		sents_indices_and_vecs =  zip(range(num_sentences), sents_embeddings)
-		all_vectors = []
-		
-		for sent_index, sent_vectors in sents_indices_and_vecs:
-		
-		        sent_vectors = []
-		        
-		        assert len(sents[sent_index]) == sent_vectors.shape[0]
-		        
-			for i, (w, vec) in enumerate(zip(sents[sent_index], sent_vectors)):   
-			
-				v = Vector(vec.detach().numpy(), sents[sent_index], i)
-				sent_vectors.append(v)
-				
-			all_vectors.append(sent_vectors)
-				
-		return all_vectors
+        def _list_vectors(self, sents_embeddings: List[np.ndarray], sents: List[List[str]], parsed_sents:  List[List[str]]) -> List[List[Vector]]:
+        
+        
+                """
+                Transform the list of all state vectors (as numpy arrays) to a list of Vector objects.
+                
+                Parameters
+                
+                ---------
+                sents_embeddings: ``List[np.ndarray]``, required
+                
+                        A list of ELMO embeddings for all sentences. Each list element is ELMO embedding 
+                        of a different sentence, with dimensions (sentence_length, 1024)
+                
+                sents: `` List[List[str]]``, required
+                
+                        A list of all sentences. Each list contains a list representing a different sentence.
+                
+                parsed_sents: `` List[List[str]]``, required
+                        
+                        A list of all dependency edges. Each list contains a list representing a different sentence.
+                        parsed_sents[i][j] contains the dependency edge between the jth word in the ith sentence,
+                        and its parent.
+                        
+                Returns
+                ---------
+                
+                all_vectors: ``List[List[Vector]``
+                       
+                       A list of lists of Vector objects. all_vectors[i][j] is the representation of the jth word
+                       in the ith sentence.
+                        
+                """
+                
+                print("Creating Vector objects required for nearest neighbor search...")
+                
+                assert len(sents) == len(sents_embeddings) == len(parsed_sents)
+                
+                num_sentences = len(sents)
+                sents_indices_and_vecs =  zip(range(num_sentences), sents_embeddings)
+                all_vectors = []
+                
+                for sent_index, sent_vectors in tqdm.tqdm(sents_indices_and_vecs):
+                
+                        sent_vectors = []
+                        
+                        assert len(sents[sent_index]) == sent_vectors.shape[0]
+                        
+                        for i, (w, dep, vec) in enumerate(zip(sents[sent_index], parsed_sents[sent_index], sent_vectors)):   
+                        
+                                v = Vector(vec.detach().numpy(), sents[sent_index], i, dep)
+                                sent_vectors.append(v)
+                                
+                        all_vectors.append(sent_vectors)
+                                
+                return all_vectors
 
-e = Evaluator()
+#e = Evaluator()
