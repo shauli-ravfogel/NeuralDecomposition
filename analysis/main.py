2e4b4faabb35933a1d1cd4a900bd41ba69b7e07b
Shauli Ravfogel <shauli321@gmail.com>
2019-06-30 14:05:40 +0300

    adding args

--- analysis/main.py
+++ analysis/main.py
@@ -1,8 +1,23 @@
 import evaluator
 import syntactic_extractor
+import args
 
 
 if __name__ == '__main__':
 
-        extractor = syntactic_extractor.CCASyntacticExtractor()
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--extractor', dest='extractor_type', type=str,
+                        default='cca',
+                        help='pca / cca')
+        
+        args = parser.parse()
+        
+        if args.extractor_type == "cca":
+        
+                        
+                extractor = syntactic_extractor.CCASyntacticExtractor()
+        else:
+                extractor = syntactic_extractor.CCASyntacticExtractor()
+                
         evaluator = evaluator.Evaluator(extractor)
+        evaluator.test()
