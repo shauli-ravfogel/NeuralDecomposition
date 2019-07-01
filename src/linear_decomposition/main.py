d19154d778f2fdbc504ffeb636f335bb6221018f
Shauli Ravfogel <shauli321@gmail.com>
2019-07-01 03:05:19 -0500

    adding arguments

--- src/linear_decomposition/main.py
+++ src/linear_decomposition/main.py
@@ -1,5 +1,6 @@
 import argparse
-
+import pca_decomp
+import cca_decomp
 
 if __name__ == '__main__':
 
@@ -10,19 +11,28 @@ if __name__ == '__main__':
                         default='pos1gram',
                         help='Type of dataset: Bert / pos0gram / pos1gram')
     parser.add_argument('--method', dest='method', type=str,
-                        default='pca',
+                        default='cca',
                         help='Method: cca / pca')
-    parser.add_argument('--dim', dest='dim', type=int,
-                        default='64',
-                        help='Number of syntactic dimensions')                        
+    parser.add_argument('--syn-dim', dest='syntactic_dim', type=int,
+                        default=100,
+                        help='Number of syntactic dimensions')  
+    parser.add_argument('--semantic_dim', dest='sem_dim', type=int,
+                        default=200,
+                        help='Number of semantic dimensions (only relevant for PCA)')                       
     parser.add_argument('--reduce-first', dest='reduce', type=bool,
                         default='True',
                         help='Whether or not to perform PCA first on the entire data, to filter out noise.')  
     parser.add_argument('--reduce-dim', dest='reduce_dim', type=int,
-                        default='900',
+                        default=900,
                         help='How many dimensions in the initial PCA?.') 
-    
+    parser.add_argument('--num_sentences', dest='num_sentences', type=int,
+                        default=15,
+                        help='How many sentences?.')     
     args = parser.parse_args()
-                                              
-    output_filename = "decomoModel." + "" if not args.reduce else "PCA:"+str(args.reduce_dim) +".Method:" + args.method + ".Dim:" + str(args.dim) + ".pickle"
-    print(output_filename)
+    
+    data = "../data/interim/bert_online_data.txt"                                              
+    output_filename = "decomoModel." + "" if not args.reduce else "PCA:"+str(args.reduce_dim) +".Method:" + args.method + ".Dim:" + str(args.syntactic_dim) + ".Data:" +args.dataset_type + ".Sentences:" + str(args.num_sentences) +  ".pickle"
+    
+    if args.method == "cca":
+    
+        decomp = cca_decomp.CCADecomposition(args.syntactic_dim, args.reduce, args.reduce_dim, args.num_sentences, data)
