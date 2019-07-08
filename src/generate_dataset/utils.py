1d4af014ece8a951da096262075146b61420d812
Shauli Ravfogel <shauli321@gmail.com>
2019-07-08 16:40:32 +0300

    adding function words

--- src/generate_dataset/utils.py
+++ src/generate_dataset/utils.py
@@ -5,16 +5,16 @@ DEFAULT_PARAMS = {"file_name": "resources/wikipedia.sample.tokenized",
                   "elmo_options": "resources/elmo_2x4096_512_2048cnn_2xhighway_options.json",
                   "elmo_weights": "resources/elmo_2x4096_512_2048cnn_2xhighway_weights.hdf5",
                   "word2vec": "resources/GoogleNews-vectors-negative300.bin",
-                  "num_sentences": 20,
+                  "num_sentences": 30,
                   "pos_tags_to_replace": ["NN", "NNS", "NNP", "NNPS", "PRP$", "JJ", "CD", "VB", "VBD", "VBG", "VBN",
                                           "VBP", "VBZ"],
-                  "function_words": ["in", "on", "at", "the", "a", "an", "who", "that", "this", "which", "can",
+                  "function_words": set(["in", "on", "at", "the", "a", "an", "who", "that", "this", "which", "can",
                                      "cannot", "not", "no", "do", "does", "of", "these", "those", ",", ".", "'", "(",
                                      ")", "under", "above", "near", "without", "with", "have", "having", "has", "as", "nor", "am",
                                      "is", "are", "although", "while", "between", "neither", "and", "or",
                                      "among", "anything", "beside", "besides", "both", "off", "did", "nothing", "now",
                                      "over", "rather", "perhaps", "sometime", "such", "upon",
-                                     "whereas", "where", "when", "who", "whom", "what", "why", "yes", "yet", "were", "was", "been", "be", "will", "would","could", "should", "amongst", "always", "along", "all", "afterwards", "'s", "during", '"', "for", "from", "to", "into", "there", "instead", "-", ":", "-", ";", "?", "about", "but", "also", "something", "out", "up", "it", "being", "just", "i", "'ve", "some", "against", "...", "'re", "much", "``", "''", "only", "least", "first", "n't", "its", "'ll", "--", "more", "such"]
+                                     "whereas", "where", "when", "who", "whom", "what", "why", "yes", "yet", "were", "was", "been", "be", "will", "would","could", "should", "amongst", "always", "along", "all", "afterwards", "after", "'s", "during", '"', "for", "from", "to", "into", "there", "instead", "-", ":", "-", ";", "?", "about", "but", "something", "out", "up", "it", "being", "just", "i", "'ve", "some", "against", "...", "'re", "much", "``", "''", "only", "least", "first", "n't", "its", "'ll", "--", "more", "such", "how", "by", "thus", "[", "]", "/", "sometime", "sometimes", "so", "even", "got", "gotten", "get", "due", "since", "because", "though", "however", "why", "off", "one", "very", "if", "until", "then", "than", "must", "through", "almost", "any", "may", "further", "less", "least", "worthy", "course", "before", "beforehand", "either", "whatever", "behalf", "well", "had", "need", "ought", "whether", "own", "according", "accordingly", "regarding", "you", "he", "mine", "our", "his", "her", "she", "my", "they", "their", "most", "!", "?", "each", "too", "once", "again", "soon", "apart", "enough", "few", "many", "forth", "thereafter", "several", "times", "ever", "simply", "specific", "per", "underneath", "beneath", "every", "er", "ed", "ing", "whole", "alone", "nearby", "within", "whom", "toward", "towards", "doesn't", "dont", "don't", "doesnt", "probably", "same", "other", "we", "nevertheless", "via", "already", "various", "still", "aftermath", "despite", "none", "i", "ii", "beyond"])
                   }
 
 
