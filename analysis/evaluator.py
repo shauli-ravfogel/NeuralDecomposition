401fad4e615e5edeedf554f3019f3ba5b3625833
Shauli Ravfogel <shauli321@gmail.com>
2019-07-08 16:50:34 +0300

    bug fix in cca application; adding closest sentence test
    - .reshape(1, -1) caused wrong input dimension to the cca extractor. replaced with [v] as it expects a batch.
    
    - added closest sentence test: represent each sentence as an average of its transformed content words, and look for closest sentence.

--- analysis/evaluator.py
+++ analysis/evaluator.py
@@ -11,7 +11,17 @@ import tqdm
 from sklearn.cluster import KMeans
 import copy
 import termcolor
-
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib
+matplotlib.use('tkagg')
+import sys
+sys.path.append('../src/generate_dataset')
+import utils
+import pylab
+                
+                
 class Vector(object):
     
     def __init__(self, vec, sentence, index, dep):
@@ -87,47 +97,141 @@ class Evaluator(object):
         
         def test(self):
         
+                #self.plot()
+
+                self.get_closest_sentence()
+
                 print("***Cloest neighbor test, before the application of the syntactic extractor***")
-                self.closest_vector_test(apply_transformation = False)
+                self.closest_vector_test(apply_transformation = False, verbose = True)
+                print("\n\n\n\n\n\n\n\n\n\n")
                 print("***Cloest neighbor test, after the application of the syntactic extractor***")
-                self.closest_vector_test(apply_transformation = True, verbose = False)
+                self.closest_vector_test(apply_transformation = True, verbose = True)
                 
-                print("***Vector-dep association test, before the application of the syntactic extractor***")
-                self.vector_dep_association_test(apply_transformation = False, verbose = False)
-                print("***Vector-dep association test, After the application of the syntactic extractor***")
-                self.vector_dep_association_test(apply_transformation = True, verbose = True)
                 
+                #print("***Vector-dep association test, before the application of the syntactic extractor***")
+                #self.vector_dep_association_test(apply_transformation = False, verbose = False)
+                #print("***Vector-dep association test, After the application of the syntactic extractor***")
+                #self.vector_dep_association_test(apply_transformation = True, verbose = False)
+                
+
+        def plot(self):
+        
+        
+                random.seed(0)
+
+                vec_lists = self.vec_lists
+                all_vecs = [v.get_vector().copy() for sent_vecs in vec_lists for v in sent_vecs if v.get_word() not in utils.DEFAULT_PARAMS["function_words"]] # all vecs in a single list.
+                #all_vecs = [v for v in all_vecs if v.get_word() not in utils.DEFAULT_PARAMS["function_words"]]
+
+                all_deps = [v.dep for sent_vecs in vec_lists for v in sent_vecs if v.get_word() not in utils.DEFAULT_PARAMS["function_words"]]
+                deps_set = set(all_deps)
+                dep2int = {d:i for i,d in enumerate(deps_set)}
+                deps = [dep2int[dep] for dep in all_deps]
+
+
+                for i,v in enumerate(all_vecs):
+                        
+                   all_vecs[i] = self.extractor.extract(v.copy().reshape(1, -1)).reshape(-1)
+                
+                k = 3500
+                
+                all_vecs = np.array(all_vecs[:k])
+                deps = deps[:k]                
+                print ("calculating projection...")
+                
+                proj = TSNE(n_components=2).fit_transform(all_vecs)
+                
+                
+                fig, ax = plt.subplots()
+                
+                xs, ys = proj[:,0], proj[:,1]
+                xs, ys = list(xs), list(ys)
+                
+
+                #ax.scatter(xs, ys, color = deps, cmap=pylab.cm.cool)
+                pylab.scatter(xs, ys, c=deps, cmap=pylab.cm.cool)
+                pylab.show()
+                #plt.show()                
+                #plt.show()
+        
+        def get_closest_sentence(self, n = 1000, apply_transformation  = False):
+        
+                random.seed(0)
+                vec_lists = self.vec_lists
+                
+                sents_repres = []
+                sents_strs = []
+                
+                for i, sent_vecs in tqdm.tqdm(enumerate(self.vec_lists)):
+                
+                        #sent_vecs_np = np.array([self.extractor.extract([v.get_vector()]).reshape(-1)[1:] for v in sent_vecs])
+                        sent_vecs_np = np.array([self.extractor.extract([v.get_vector()]).reshape(-1)[:] for v in sent_vecs])
+                        #sent_vecs_np = np.stack([v.get_vector()[1:] for v in sent_vecs])
+                        sent_lst = sent_vecs[0].sentence#.split(" ")
+                        good_indices = np.array([i for i in range(len(sent_lst)) if sent_lst[i] not in utils.DEFAULT_PARAMS["function_words"]])
+                        sent_vecs_np = sent_vecs_np[good_indices]
+                        sent_mean_vec = np.mean(sent_vecs_np, axis = 0)
+                        sents_repres.append(sent_mean_vec)
+                        sents_strs.append(sent_vecs[0].get_sentence())
+                
+                sents_repres = np.array(sents_repres)
+                for i in range(len(sents_repres)):
+                
+                        sents_repres[i] /= np.linalg.norm(sents_repres[i])
+                        
+                print(sents_repres[0].shape)
+                
+                for i in range(min(n, len(self.vec_lists))):
+                
+                        similarity_scores = sents_repres.dot(sents_repres[i].T)
+                        three_largest = similarity_scores.argsort()[-3:][::-1]
+                        j = three_largest[1]
+                        sent_i, sent_j = sents_strs[i], sents_strs[j]
+                        
+                        print(" ".join(sent_i))
+                        print("-------------------")
+                        print(" ".join(sent_j))
+                        print("====================================================")
+                        
+                                
+                
+                        
+                                                        
         def closest_vector_test(self, n = 1000, verbose = False, apply_transformation = False):
         
+                random.seed(0)
+                
                 print("Performing closest vector test...")
                 
                 vec_lists = self.vec_lists
                 all_vecs = [copy.deepcopy(v) for sent_vecs in vec_lists for v in sent_vecs] # all vecs in a single list.
-                
+                all_vecs = [v for v in all_vecs if v.get_word() not in utils.DEFAULT_PARAMS["function_words"]]
+                                
                 if apply_transformation:
                 
                         for i,v in enumerate(all_vecs):
                         
-                               v.vec = self.extractor.extract(v.vec.copy().reshape(1, -1)).reshape(-1)
+                               v.vec = self.extractor.extract([v.vec.copy()]).reshape(-1)
                 
                 random.shuffle(all_vecs)
                 
                 good, bad = 0., 0.
                 
                 
-                for i in range(n):
+                for i in range(min(len(all_vecs), 1000)):
                 
                         vec = all_vecs[i]
                         nearest_neighbor = Vector.get_closest_vector(vec, all_vecs)
+                        correct = False
                         
                         if vec.dep == nearest_neighbor.dep:
-                        
+                                correct = True
                                 good += 1
                         
                         else:
                                 bad += 1
                         
-                        if verbose:
+                        if verbose and i < 300:
                         
                                 print("key: {} \n value: {}\n key-dep: {} \n value-dep: {} \n ------------------------------------------------".format(vec, nearest_neighbor, vec.dep, nearest_neighbor.dep))
                         
@@ -136,11 +240,14 @@ class Evaluator(object):
         
         def vector_dep_association_test(self, n = 1000, clustering = "kmeans", num_clusters = 50, verbose = False, apply_transformation = False):
         
+                random.seed(0)
                 print("Perfoming vector-dependency association test")
                 print("Performing clustering...")
                 
                 vec_lists = self.vec_lists
-                all_vecs = [copy.deepcopy(v.get_vector()) for sent_vecs in vec_lists for v in sent_vecs] # all vecs in a single list.
+                all_vecs = [v.get_vector().copy() for sent_vecs in vec_lists for v in sent_vecs if v.get_word() not in utils.DEFAULT_PARAMS["function_words"]] # all vecs in a single list.
+                #all_vecs = [v for v in all_vecs if v.get_word() not in utils.DEFAULT_PARAMS["function_words"]]
+                
                 if apply_transformation:
                 
                         for i,v in enumerate(all_vecs):
@@ -208,6 +315,7 @@ class Evaluator(object):
                 options_file = "../data/external/elmo_2x4096_512_2048cnn_2xhighway_options.json"
                 weight_file = "../data/external/elmo_2x4096_512_2048cnn_2xhighway_weights.hdf5"
                 return ElmoEmbedder(options_file, weight_file, cuda_device=0)
+                return ElmoEmbedder(options_file, weight_file)
         
         def _run_elmo(self, sentences: List[List[str]]) -> List[np.ndarray]:
         
@@ -228,7 +336,7 @@ class Evaluator(object):
                 return all_embeddings
                 
                 
-        def _load_sents(self, fname = "sents_f", max_length = 25) -> List[List[str]]:
+        def _load_sents(self, fname = "sents_f", max_length = 35) -> List[List[str]]:
         
                 print("Loading sentences...")
                 
@@ -239,7 +347,7 @@ class Evaluator(object):
                 if max_length is not None:
                         lines = list(filter(lambda sentence: len(sentence) < max_length, lines))
                 
-                lines = lines[:]
+                lines = lines[:45000]
                 
                 return lines
                 
