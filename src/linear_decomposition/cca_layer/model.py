11daed316e00c907fb24b3008ecb1c0dc5ff6c25
shauli1321 <shauli321@gmail.com>
2019-07-24 09:56:39 +0300

    training and evaluation functions

--- src/linear_decomposition/cca_layer/model.py
+++ src/linear_decomposition/cca_layer/model.py
@@ -8,22 +8,38 @@ import copy
 
 class ProjectionNetwork(nn.Module):
 
-    def __init__(self, dim = 1024):
+    def __init__(self, dim = 2048):
 
         super(ProjectionNetwork, self).__init__()
 
         layers = []
-        layers.append(nn.Linear(dim, 1024, bias=True))
+        layers.append(nn.Linear(dim, 128))
         layers.append(nn.ReLU())
-        layers.append(nn.Linear(1024, 512))
+        layers.append(nn.Linear(128,128))
+        self.linear = nn.Linear(dim, 5)
 
         self.layers = nn.Sequential(*layers)
         self.cca = cca_layer.CCALayer()
 
-    def forward(self, X, Y, r=1e-6):
+    def forward(self, X, Y):
 
-        X, Y = self.layers(X), self.layers(Y)
-        X_projected, Y_projected = self.cca(X,Y)
+        #print("---------------------------")
+
+        #X_h = self.layers(X)
+        #Y_h = self.layers(Y)
+        X_h, Y_h = self.linear(X), self.linear(Y)
+        #X_h, Y_h = torch.mm(X, self.w), torch.mm(Y, self.w)
+        #X_h, Y_h = X, Y
+        #print("X before CCA layer:\n")
+        #print(X_h, X_h.shape)
+        #print("Y before CCA layer:\n")
+        #print(Y_h)
+        X_projected, Y_projected = self.cca(X_h,Y_h)
+
+        #print("X after CCA :\n")
+        #print(X_projected)
+        #print("Y after CCA :\n")
+        #print(Y_projected)
 
         return X_projected, Y_projected
 
