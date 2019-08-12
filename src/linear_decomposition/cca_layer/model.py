e6fd46acce38c5f543853294b1b36a91fd443ba0
shauli1321 <shauli321@gmail.com>
2019-08-12 09:37:57 +0300

    position adverserial loss

--- src/linear_decomposition/cca_layer/model.py
+++ src/linear_decomposition/cca_layer/model.py
@@ -5,47 +5,65 @@ from torch import nn
 import cca_layer
 import tqdm
 import copy
+from pytorch_revgrad import RevGrad
 
 class ProjectionNetwork(nn.Module):
 
-    def __init__(self, dim = 2048, final_dim  = 512):
+    def __init__(self, dim = 2048, final = 50):
 
         super(ProjectionNetwork, self).__init__()
 
         layers = []
         layers.append(nn.Linear(dim, 1024))
-        layers.append(nn.ReLU())
-        layers.append(nn.Linear(1024, final_dim))
-        #layers.append(nn.ReLU())
-        #layers.append(nn.Linear(512, 512))
+        #layers.append(nn.LeakyReLU())
+        layers.append(nn.Linear(1024, 512))
+        #layers.append(nn.LeakyReLU())
+        layers.append(nn.Linear(512, 512))
+        #layers.append(nn.Dropout(0.5))
+        layers.append(nn.Linear(512, final))
+        #layers.append(nn.Dropout(0.5))
+        #layers.append(nn.LayerNorm(512))
+        #layers.append(nn.Softsign())
+        #layers.append(nn.Dropout(0.1))
+
 
         self.W = torch.nn.Parameter(0.0001 * (torch.randn(dim, 100) - 0.5))
 
         self.layers = nn.Sequential(*layers)
-        self.cca = cca_layer.CCALayer(dim, final_dim)
+        self.cca = cca_layer.CCALayer()
+
+        pos_network = []
+        pos_network.append(RevGrad())
+        pos_network.append(nn.Linear(final, 512))
+        #pos_network.append(nn.ReLU())
+        pos_network.append(nn.Linear(512, 256))
+        #pos_network.append(nn.ReLU())
+        pos_network.append(nn.Linear(256, 128))
+        #pos_network.append(nn.ReLU())
+        pos_network.append(nn.Linear(128, 50))
+
+        self.pos_net = nn.Sequential(*pos_network)
 
     def forward(self, X, Y):
 
-        #print("---------------------------")
-
-        #X_h = self.layers(X)
-        #Y_h = self.layers(Y)
-
         X_h, Y_h = self.layers(X), self.layers(Y)
+        #X_h *= 1e-1
+        #Y_h *= 1e-1
         #X_h, Y_h = torch.mm(X, self.W), torch.mm(Y, self.W)
         #X_h, Y_h = X, Y
         #print("X before CCA layer:\n")
         #print(X_h, X_h.shape)
         #print("Y before CCA layer:\n")
         #print(Y_h)
-        T, (X_projected, Y_projected) = self.cca(X_h,Y_h, is_training = self.training)
+        total_corr, (X_projected, Y_projected) = self.cca(X,Y, is_training = self.training)
 
         #print("X after CCA :\n")
         #print(X_projected)
         #print("Y after CCA :\n")
         #print(Y_projected)
 
-        return T, (X_projected, Y_projected)
+        #if np.random.random() < 1e-1: print(X_h[0][:20])
+        return total_corr, (X_projected, Y_projected), self.pos_net(X_h)
 
 if __name__ == '__main__':
 
