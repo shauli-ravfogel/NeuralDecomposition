5ccf7cd7dead1b4bd324e7f3ef4d27b424cd8eb7
Shauli Ravfogel <shauli321@gmail.com>
2019-08-14 16:22:50 +0300

    main for views collection

---
+++ src/linear_decomposition/collect_views.py
@@ -0,0 +1,43 @@
+import argparse
+import views_collector
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Views collection',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--input-path', dest='input_path', type=str,
+                        default='sample.hdf5',
+                        help='name of the hdf5 input file (containing encoded equivalent sentences)')
+    
+    parser.add_argument('--output-path', dest='output_path', type=str,
+                        default='out.pickle',
+                        help='name of the output file (containing the collected views)')
+
+    parser.add_argument('--num_examples', dest='num_examples', type=int,
+                        default = 300,
+                        help='how many pairs to collect')
+                            
+    parser.add_argument('--mode', dest='mode', type=str,
+                        default = "simple",
+                        help='simple / averaged / sentence-level')
+
+    parser.add_argument('--exclude_function_words', dest='exclude_function_words', type=bool,
+                        default =True,
+                        help='whether or not to exclude function words from the pairs')
+
+
+    args = parser.parse_args()
+    
+    if args.mode == "simple":
+    
+        collector = views_collector.SimpleCollector(args.input_path, args.num_examples, args.output_path, exclude_function_words = args.exclude_function_words)
+    
+    elif args.mode == "averaged":
+    
+        collector = views_collector.AveragedCollector(args.input_path, args.num_examples, args.output_path, exclude_function_words = args.exclude_function_words)  
+         
+    elif args.mode == "sentence-level":
+    
+        collector = views_collector.SentenceCollector(args.input_path, args.num_examples, args.output_path, exclude_function_words = args.exclude_function_words) 
+         
+    collector.collect_views()
