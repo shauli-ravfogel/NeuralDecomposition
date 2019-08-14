ecb62702aa6e99cb978dc88749cf0264fbff269f
Shauli Ravfogel <shauli321@gmail.com>
2019-08-14 17:28:21 +0300

    prevent multiple collections of the same function word

--- src/linear_decomposition/views_collector.py
+++ src/linear_decomposition/views_collector.py
@@ -11,7 +11,7 @@ Equivalent_sentences_group = typing.NamedTuple("equivalent_sentences",
                                      
 class CollectorBase(object):
 
-        def __init__(self, path, view_size, output_filename, exclude_function_words = True):
+        def __init__(self, path, view_size, output_filename, method, exclude_function_words = True):
         
                 """
                 Parameters
@@ -27,6 +27,7 @@ class CollectorBase(object):
                 self.view_size = view_size
                 self.output_filename = output_filename
                 self.exclude_function_words = exclude_function_words
+                self.method = method
         
         def read_one_group(equivalent_sentences: Equivalent_sentences_group) -> List[Tuple[np.ndarray, np.ndarray, int]]:
         
@@ -60,9 +61,11 @@ class CollectorBase(object):
                         pbar.update(1)
                         i += 1
                
-                print("Collected {} instances from {} sentences".format(len(views), i))
+                print("Collected {} pairs from {} sentences".format(len(views), i))
                 
-                with open("output_filename", "wb") as f:
+                output_filename = "views.sentences:{}.pairs:{}.mode:{}.no-func-words:{}".format(i, len(views), self.method, self.exclude_function_words)
+                
+                with open(output_filename, "wb") as f:
                 
                         pickle.dump(views, f)
                 
@@ -76,60 +79,62 @@ class CollectorBase(object):
 
 class SimpleCollector(CollectorBase):
 
-        def __init__(self, path, view_size, output_filename, exclude_function_words=True):
-        
-        
-                super(SimpleCollector, self).__init__(path, view_size, output_filename, exclude_function_words)
+        def __init__(self, *args):
+             
+                super(SimpleCollector, self).__init__(*args)
                 
         def read_one_group(self, vecs: np.ndarray, sents: np.ndarray, content_idx: np.ndarray, sent_len: int, group_size: int) -> List[Tuple[np.ndarray, np.ndarray, int]]:
                                 
-                view1 = vecs[::1,:, :]  #(num sents/2+-1, num_indices, 2048)
-                view2 = vecs[1::1,:, :] # (num_sents/2+-1, num_indices, 2048)
-                view1_words = sents[::1,:] #(num_sents/2+-1, sent_length)
-                view2_words = sents[1::1,:] # (num_sents/2+-1, sent_length
+                view1 = vecs[:,:, :]  #(num sents, num_indices, 2048)
+                view2 = vecs[1:,:, :] # (num_sents-1, num_indices, 2048)
+                view1_words = sents[:,:] #(num_sents, sent_length)
+                view2_words = sents[1:,:] # (num_sents-1, sent_length
                 
                 data = []
                 
                 for word_index in range(sent_len):
                 
-                        if word_index not in content_idx: continue
+                        is_function_word = word_index not in content_idx
+                        if self.exclude_function_words and is_function_word: continue
                         
-                        view1_vecs = view1[:, word_index, :]
-                        view2_vecs = view2[:, word_index, :]
-                        view1_words_at_index = view1_words[:, word_index]
-                        view2_words_at_index = view2_words[:, word_index]
+                        view1_vecs = view1[:, word_index, :] # (group_size, 2048)
+                        view2_vecs = view2[:, word_index, :] # (group_size - 1, 2048)
+                        view1_words_at_index = view1_words[:, word_index] # (group_size,)
+                        view2_words_at_index = view2_words[:, word_index] # (group_size - 1,)
                         idx = [word_index] * min(view1_vecs.shape[0], view2_vecs.shape[0])
                         examples = list(zip(view1_vecs, view2_vecs, view1_words_at_index, view2_words_at_index, idx))
-                        data.extend(examples)
-                
+                        
+                        if self.exclude_function_words or (not is_function_word):                 
+                                data.extend(examples)
+                        else: # function word & we include function words
+                                data.append(examples[0]) # don't append multiple occurrences of the same function word.
+                        
+                                
                 return data
                       
                                 
-                      
-        
-        
         
         
         
 class AveragedCollector(CollectorBase):
 
-        def __init__(self, path, view_size, output_filename, exclude_function_words=True):
+        def __init__(self, *args):
         
         
-                super(AveragedCollector, self).__init__(path, view_size, output_filename, exclude_function_words)
+                super(AveragedCollector, self).__init__(*args)
                 
         def read_one_group(self, vecs: np.ndarray, sents: np.ndarray, content_idx: np.ndarray, sent_len: int, group_size: int) -> List[Tuple[np.ndarray, np.ndarray, int]]:
                                 
-                view1 = vecs[::1,:, :]  #(num sents/2+-1, num_indices, 2048)
-                view2 = vecs[1::1,:, :] # (num_sents/2+-1, num_indices, 2048)
-                view1_words = sents[::1,:] #(num_sents/2+-1, sent_length)
-                view2_words = sents[1::1,:] # (num_sents/2+-1, sent_length
+                view1 = vecs[::2,:, :]  #(num sents/2 +-1, num_indices, 2048)
+                view2 = vecs[1::2,:, :] # (num_sents/2 +-1, num_indices, 2048)
+                view1_words = sents[::2,:] #(num_sents/2 +-1, sent_length)
+                view2_words = sents[1::2,:] # (num_sents/2 +- 1, sent_length
                 
                 data = []
                 
                 for word_index in range(sent_len):
                 
-                        if word_index not in content_idx: continue
+                        if self.exclude_function_words and (word_index not in content_idx): continue
                         
                         view1_vecs = view1[:, word_index, :]
                         view2_vecs = view2[:, word_index, :]
@@ -138,24 +143,23 @@ class AveragedCollector(CollectorBase):
                         m2 = np.mean(view2_vecs, axis = 0)[None, :]
 
                         data.append((m1, m2, word_index))
-                
+                                
                 return data
                                
         
         
 class SentenceCollector(CollectorBase):
 
-        def __init__(self, path, view_size, output_filename, exclude_function_words=True):
+        def __init__(self, *args):
         
-        
-                super(SentenceCollector, self).__init__(path, view_size, output_filename, exclude_function_words)
+                super(SentenceCollector, self).__init__(*args)
                 
         def read_one_group(self, vecs: np.ndarray, sents: np.ndarray, content_idx: np.ndarray, sent_len: int, group_size: int) -> List[Tuple[np.ndarray, np.ndarray, int]]:
                                 
-                view1 = vecs[::1,:, :]  #(num sents+-1, num_indices, 2048)
-                view2 = vecs[1::1,:, :] # (num_sents+-1, num_indices, 2048)
-                view1_words = sents[::1,:] #(num_sents+-1, sent_length)
-                view2_words = sents[1::1,:] # (num_sents+-1, sent_length
+                view1 = vecs[:,:, :]  #(num sents, num_indices, 2048)
+                view2 = vecs[1:,:, :] # (num_sents-1, num_indices, 2048)
+                view1_words = sents[:,:] #(num_sents, sent_length)
+                view2_words = sents[1:,:] # (num_sents-1, sent_length
                 
                 data = []
                 
