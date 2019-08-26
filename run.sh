20a146bbde3e27fa309cf90ac897d7d53c133301
Yanai Elazar <yanaiela@gmail.com>
2019-08-26 17:54:29 +0300

    adaptable analysis and structural changes (moved all code into src)

--- run.sh
+++ run.sh
@@ -36,10 +36,13 @@ python src/linear_decomposition/main.py \
         --views-file-path data/interim/views/views.sentences:7.pairs:1008.mode:simple.no-func-words:True \
         --perform-pca 0 --cca-dim 100 \
         --enforce-symmetry 1 --cca-model numpy
-# the above script creates a CCA model in the ./models directory. The file is named according to the arguments (cca-dim etc.)
+# the above script creates a CCA model in the models directory. The file is named according to the arguments (cca-dim etc.)
 
 ### evaluation
 
-cd ../../analysis
-python3 main.py --input-wiki ../data/external/wiki.clean.250k --encode_sentences 0 --encoded_data ../data/interim/encoded_sents.pickle --elmo_folder ../data/external --method cosine --cuda-device 0 --num_sents 25000 --num_words 100000 --num_queries 5000 --extractor numpy_cca --extractor_path ../src/linear_decomposition/models/cca_model_name
-# Yanai, the above script run on ELMO states. to Run on BERT states, you need to (1) use extractor trained on BERT states (2) implement embedder.py for BERT (or modify your BERT states collection script so that it will also work on the format of  wiki.clean.250k).
+# run analysis on the produced embeddings.
+python src/analysis/main.py --input-wiki data/external/wiki.clean.250k \
+        --encode_sentences 0 --encoded_data data/interim/encoded_sents.pickle \
+        --elmo_folder data/external --method cosine --cuda-device 0 \
+        --num_sents 25000 --num_words 100000 --num_queries 5000 --extractor numpy_cca \
+        --extractor_path data/processed/models/cca.perform-pca:True.cca-dim:100.symmetry:True.pickle
