bug fix in loss calculation; normalization in last layer

src/deep_soft_cca/dataset.py
@@ -22,5 +22,5 @@ class Dataset(data.Dataset):
 
         with torch.no_grad():
 
-            word_repres = self.data[index]
+            word_repres = self.data[index]["vecs"]
             return [torch.from_numpy(w).float() for w in word_repres]

src/deep_soft_cca/loss.py
@@ -12,7 +12,7 @@ class SoftCCALoss(torch.nn.Module):
         super(SoftCCALoss, self).__init__()
         self.p = p
 
-    def forward(self, X, Y, r = 1e-4, alpha = 1e-0):
+    def forward(self, X, Y, r = 1e-4, alpha = 1e-1):
 
         m1 = torch.mean(X, dim=0, keepdim=True)
         m2 = torch.mean(Y, dim=0, keepdim=True)
@@ -24,12 +24,21 @@ class SoftCCALoss(torch.nn.Module):
         S22 = ((torch.t(Y)  @ Y) / (N - 1)) + r * torch.eye(d).float()
         S12 = (torch.t(Y) @ X) / (N - 1)
 
-        corr_term = 0.5 * torch.norm(X - Y, p = "fro", dim = 1)**2
-        # zero diagonals in covariances matrices
+        """ 
+        print(m1, m1.shape)
+        print("----------------------------------")
+        print(X, X.shape)
+        print("----------------------------------")
+        print(S11, S11.shape)
+        #exit()
+        """
 
+        corr_term = 0.5 * torch.norm(X - Y, p = "fro")**2
+
+        # zero diagonals in covariances matrices
         S11 = S11 - torch.eye(S11.shape[0])
         S22 = S22 - torch.eye(S22.shape[0])
 
         decorrelation_term = torch.norm(S11, p = 1) + torch.norm(S22, p = 1)
-        loss = corr_term.mean() + (1./d**2) * alpha * decorrelation_term
+        loss = corr_term +  alpha * decorrelation_term # * (1./d**2)
         return loss

src/deep_soft_cca/training.py
@@ -19,8 +19,7 @@ def train(model, training_generator, dev_generator, loss_fn, optimizer, num_epoc
             if (loss < best_loss) or 0:
                 best_loss = loss
                 torch.save(model.state_dict(), "NeuralCCAStateDict.pickle")
-                with open("NeuralCCA.pickle", "wb") as f:
-                    pickle.dump(model,f)
+                torch.save(model, "NeuralCCA.pickle")
         print()
         print("Loss: {}".format(loss))
         print("\nEpoch {}. Best accuracy so far is {}".format(epoch, best_loss))
@@ -30,7 +29,7 @@ def train(model, training_generator, dev_generator, loss_fn, optimizer, num_epoc
         t = tqdm.tqdm(iter(training_generator), leave=False, total=len(training_generator), ascii = True)
         i = 0
 
-        for (w1,w2,w3,w4,w5,w6, w7, w8, w9, w10) in t:
+        for (w1,w2,w3,w4) in t:
 
             view1, view2 = w1, w3
             X,Y =  model(view1,view2)
@@ -52,7 +51,7 @@ def evaluate(model, loss_fn, dev_generator):
     loss_vals = []
     norms = []
 
-    for (w1,w2,w3,w4,w5,w6, w7, w8, w9, w10) in t:
+    for (w1,w2,w3,w4) in t:
 
         with torch.no_grad():
 