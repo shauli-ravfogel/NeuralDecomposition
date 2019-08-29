fd6c49ff305c02886fad0689a6b724ffc67eda6c
shauli1321 <shauli321@gmail.com>
2019-08-29 09:25:27 +0300

    min and max length; function words

--- src/triplet/collect_data.py
+++ src/triplet/collect_data.py
@@ -11,28 +11,33 @@ Equivalent_sentences_group = typing.NamedTuple("equivalent_sentences",
                                     [('vecs', np.ndarray), ('sents', List[List[str]]),
                                      ("content_indices", List[int])])
 
-def collect_data(path, num_examples, num_examples_per_group):
+def collect_data(path, num_examples, num_examples_per_group,  min_length = 12, max_length = 30):
 
     f = h5py.File(path, 'r')
     pbar = tqdm.tqdm(total=num_examples)
     data = []
     i = 0
+    num_sents = 150000
     keys = list(f.keys())
+    output_filename = "data_with_strs.pickle"
 
     print("Collecting data...")
-
     while len(data) < num_examples:
 
-        j,k = np.random.choice(keys, size = 2, replace = False)
-        group1, group2 = f[j], f[k]  # group has the same interface as Equivalent_sentences_group
-        group_data = generate_training_instances(group1, group2, num_examples_per_group)
+        j = keys[i % num_sents]
+        group = f[j]# group has the same interface as Equivalent_sentences_group
+        sent_length = len(group["sents"][0])
+        if sent_length < min_length or sent_length > max_length: continue
+        group_data = generate_training_instances(group, num_examples_per_group, i%num_sents)
         data.extend(group_data)
         pbar.update(len(group_data))
         i += 1
 
-    print("Collected {} instances from {} sentences".format(len(data), i))
+        if i % 1000 == 0 and i > 0:
+            with open(output_filename, "wb") as f2:
+                pickle.dump(data, f2)
 
-    output_filename = "data.pickle"
+    print("Collected {} instances from {} sentences".format(len(data), i))
 
     with open(output_filename, "wb") as f:
         pickle.dump(data, f)
@@ -40,41 +45,38 @@ def collect_data(path, num_examples, num_examples_per_group):
     f.close()
 
 
-def generate_training_instances(group1: Equivalent_sentences_group, group2: Equivalent_sentences_group, num_examples_per_group: int, filter_func_words=True):
+def generate_training_instances(group: Equivalent_sentences_group, num_examples_per_group: int, sent_id, filter_func_words=True):
 
-    vecs1, sents1, content_idx1 = group1["vecs"], group1["sents"], group1["content_indices"]
-    vecs2, sents2, content_idx2 = group2["vecs"], group2["sents"], group2["content_indices"]
-    group_size, sent1_len, sent2_len = group1.attrs["group_size"], group1.attrs["sent_length"], group2.attrs["sent_length"]
+    vecs, sents, content_idx = group["vecs"], group["sents"], group["content_indices"]
+    group_size, sent_len = group.attrs["group_size"], group.attrs["sent_length"]
 
     data = []
 
     for ind in range(num_examples_per_group):
-        # sample indices for sentences in both groups.
 
         i, j = np.random.choice(range(group_size), size = 2, replace = False)
-        k = np.random.choice(range(group_size))
-
 
         # sample word indices
 
-        l, m = np.random.choice(range(min(sent1_len, sent2_len)), size = 2, replace = True)
-        if (sents1[i,l] == sents1[j, l]) or (sents1[i,m] == sents1[j,m]): continue
+        l, m = np.random.choice(range(sent_len), size = 2, replace = True)
+
+        if l > m:
+
+            l,m = m,l
+
+        #if (sents1[i,l] == sents1[j, l]) or (sents1[i,m] == sents1[j,m]): continue
+        sent_i_str = " ".join(sents[i][:l]) + " *" + sents[i, l] + "* " + " ".join(sents[i][l + 1:m]) + " *" + sents[i, m] + "* " + " ".join(sents[i, m + 1:])
+        sent_j_str = " ".join(sents[j][:l]) + " *" + sents[j, l] + "* " + " ".join(sents[j][l + 1:m]) + " *" + sents[j, m] + "* " + " ".join(sents[j, m + 1:])
 
 
-        w1, w2 = vecs1[i,l], vecs1[j, m]
-        w3, w4 = vecs1[j, l], vecs1[i, m]
-        w5, w6 = vecs2[k,l], vecs2[k, m]
 
+        w1, w2 = vecs[i,l], vecs[j, m]
+        w3, w4 = vecs[j, l], vecs[i, m]
 
-        if ind == 0:
-            print("\n", " ".join(sents1[i]), "\n\n", " ".join(sents1[j]), "\n\n", " ".join(sents2[k]))
-            print(i,j,k,l,m)
-            print(sents1[i,l], sents1[j,m])
-            print (sents1[j,l], sents1[i,m])
-            print(sents2[k,l], sents2[k,m])
+        word1_is_function_word = l not in content_idx
+        word2_is_function_word = m not in content_idx
 
-            print("---------------------------------------------------------")
-        instance =  (w1, w2, w3, w4, w5, w6)
+        instance =  {"vecs": (w1, w2, w3, w4), "sent1":  sent_i_str, "sent2": sent_j_str, "indices": (l,m), "sent_id": sent_id, "word1_is_function": word1_is_function_word, "word2_is_function": word2_is_function_word}
         data.append(instance)
 
     return data
@@ -82,4 +84,5 @@ def generate_training_instances(group1: Equivalent_sentences_group, group2: Equi
 
 if __name__ == '__main__':
 
-    collect_data("sample.hdf5", 22000, 100)
+    #collect_data("sample.hdf5", 22000, 100)
+    collect_data("../../data/interim/encoded_sents.150k.hdf5", 2100000, 25)
