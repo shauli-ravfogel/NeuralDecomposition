d22d31ab1ac92b7e2b191ee8a1ad76fb94bd71e1
Shauli Ravfogel <shauli321@gmail.com>
2019-08-29 20:19:27 +0300

    code organization

--- src/linear_decomposition/numpy_cca.py
+++ src/linear_decomposition/numpy_cca.py
@@ -16,7 +16,7 @@ class CCAModel(object):
         self.mean_x, self.mean_y, self.A, self.B, self.sum_crr = None, None, None, None, None
         self.dim = dim
 
-    def __call__(self, H1, H2=None, training=True, alternative=True, r=1e-5, noise = True):
+    def __call__(self, H1, H2=None, training=True, r=1e-5, noise = True):
 
         # H1 and H2 are featurs X num_points matrices containing samples columnwise.
         # dim is the desired dimensionality of CCA space.
@@ -43,8 +43,6 @@ class CCAModel(object):
             H1, H2 = H1.T, H2.T
 
             S11 = ((H1.dot(H1.T)) / (N - 1)) + r * np.eye(H1.shape[0])  # cov_xx
-
-
             S22 = ((H2.dot(H2.T)) / (N - 1)) + r * np.eye(H1.shape[0])  # cov_yy
             S12 = H1.dot(H2.T) / (N - 1)  # cov_yx
 
@@ -54,7 +52,6 @@ class CCAModel(object):
             D2, V2 = la.eigh(S22)
 
             K11 = V1.dot(np.diag(1 / np.sqrt(D1))).dot(V1.T)
-
             K22 = V2.dot(np.diag(1 / np.sqrt(D2))).dot(V2.T)
 
             # Calculate correlation matrix
@@ -62,34 +59,29 @@ class CCAModel(object):
             T = K22.dot(S12).dot(K11)
 
             # Perform SVD on correlation matrix
+            # compute TT' and T'T (regularized)
+            Tnp = K11.dot(S12).dot(K22)
+            M1 = Tnp.dot(Tnp.T)
+            M2 = Tnp.T.dot(Tnp)
 
-            if not alternative:
-                U, D, V = np.linalg.svd(T)
-                self.corr = np.mean(D)
-                U, V = U[:self.dim, :], V[:self.dim, :]
-                D = np.diag(D)  # correlation coefficiens of the canonical components
+            # regularize the matrices to prevent ill-conditioning
+            
+            M1 += r * np.eye(M1.shape[0]) 
+            M2 += r * np.eye(M2.shape[0])
 
-            else:
-
-                # compute TT' and T'T (regularized)
-                Tnp = K11.dot(S12).dot(K22)
-                M1 = Tnp.dot(Tnp.T)
-                M2 = Tnp.T.dot(Tnp)
-
-                M1 += r * np.eye(M1.shape[0])
-                M2 += r * np.eye(M2.shape[0])
-
-                # compute eigen decomposition
-                E1, V = la.eigh(M1)
-                _, U = la.eigh(M2)
-                D = np.sqrt(np.clip(E1, 1e-7, 1.))
-                self.D = D
-                self.corr = np.mean(D[-self.dim:])
-                U, V = U.T[-self.dim:, :], V.T[-self.dim:, :]
+            # compute eigen decomposition
+            E1, V = la.eigh(M1)
+            _, U = la.eigh(M2)
+            D = np.sqrt(np.clip(E1, 1e-7, 1.))
+            self.D = D
+            self.corr = np.mean(D[-self.dim:])
+            U, V = U.T[-self.dim:, :], V.T[-self.dim:, :]
 
             A = K11.dot(V.T)  # projection matrix for H1
             B = K22.dot(U.T)  # projection matrix for H2
 
+            # make sure the projections of the two views will align in the sign
+            
             s = np.sign(np.diag(V.dot(S12).dot(U.T)))
             B *= s
             self.A, self.B = A, B
