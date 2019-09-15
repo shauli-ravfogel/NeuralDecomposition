f4a714c33492afb98be4ee3557322be23a9e1396
Shauli Ravfogel <shauli321@gmail.com>
2019-09-15 15:56:37 +0300

    bert model

--- src/generate_dataset/model.py
+++ src/generate_dataset/model.py
@@ -29,6 +29,75 @@ class Elmo(ModelInterface):
         return vecs
 
 
+class Bert(ModelInterface):
+
+        def __init__(self, cuda_device, layers=[1, 16, -1]):
+        
+                self.cuda_device = cuda_device
+                self.tokenizer = BertTokenizer.from_pretrained('bert-large-uncased-whole-word-masking')
+                self.model = BertForMaskedLM.from_pretrained('bert-large-uncased-whole-word-masking', output_hidden_states = True, output_attentions = True)
+                #self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+                #self.model = BertForMaskedLM.from_pretrained('bert-base-uncased', output_hidden_states = True, output_attentions = True)
+                self.model.eval()
+                self.model.to('cuda:{}'.format(self.cuda_device))
+                self.layers = layers                
+
+        def _tokenize(self, original_sentence: List[str]) -> Tuple[List[str], Dict[int, int]]:
+    
+                """
+                Parameters
+                ----------
+                Returns
+                -------
+                bert_tokens: The sentence, tokenized by BERT tokenizer.
+                orig_to_tok_map: An output dictionary consisting of a mapping (alignment) between indices in the original tokenized sentence, and indices in the sentence tokenized by the BERT tokenizer. See https://github.com/google-research/bert
+                """
+
+                bert_tokens = ["[CLS]"]
+                orig_to_tok_map = {}
+                has_subwords = False
+                is_subword = []
+        
+                for i, w in enumerate(original_sentence):
+            
+                        tokenized_w = self.tokenizer.tokenize(w)
+                        has_subwords = len(tokenized_w) > 1
+                        is_subword.append(has_subwords)
+                        bert_tokens.extend(tokenized_w)
+            
+                        orig_to_tok_map[i] = len(bert_tokens) - 1
+                  
+                bert_tokens.append("[SEP]")
+        
+                return (bert_tokens, orig_to_tok_map)
+
+        def run(self, sents: np.ndarray):
+        
+                num_sents, sent_len = sents.shape
+                embeddings = np.zeros((num_sents, sent_len, len(self.layers) * 1024))
+                
+                for i, sent in enumerate(sents):
+                
+                        bert_tokens, orig_to_tok_map = self._tokenize(sent)
+                        indexed_tokens = self.tokenizer.convert_tokens_to_ids(bert_tokens)
+                        tokens_tensor = torch.tensor([indexed_tokens]).to('cuda:{}'.format(self.cuda_device))
+                        # Predict hidden states features for each layer
+                        with torch.no_grad():
+
+                                all_hidden_states, all_attentions = self.model(tokens_tensor)[-2:]
+                                #last_hidden = all_hidden_states[-1].squeeze()#.detach().cpu().numpy()
+                                #first_hidden = all_hidden_states[1].squeeze()#.detach().cpu().numpy()
+                                #middle_hidden = all_hidden_states[int(len(all_hidden_states)/2)].squeeze()#.detach().cpu().numpy()
+                                
+                                layers = [all_hidden_states[layer].squeeze() for layer in self.layers]
+                                vecs = torch.cat(layers, dim = 1).detach().cpu().numpy()
+
+                                for j in range(sent_len):  
+                                        
+                                        embeddings[i,j] = vecs[orig_to_tok_map[j]]
+  
+                return embeddings
+
 class ElmoRandom(ModelInterface):
 
     def __init__(self, elmo_options, elmo_weights, cuda_device, rand_emb, rand_lstm, layers):
