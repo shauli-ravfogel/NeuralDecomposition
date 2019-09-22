c5b746ec64ae3fbd439ca87404a19859a4cad1f0
Yanai Elazar <yanaiela@gmail.com>
2019-09-22 22:30:34 +0300

    dependency few shot parsing

---
+++ src/dep_few_shots/data_creation/create_few_shot.py
@@ -0,0 +1,52 @@
+"""
+python create_few_shot.py --input-train /home/lazary/workspace/thesis/NeuralDecomposition/data/parsing/train
+"""
+import argparse
+import os
+import numpy as np
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Equivalent sentences generator',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--input-train', dest='input_train', type=str,
+                        help='name of training data file of the penn treebank data')
+    parser.add_argument('--data-size', dest='data_size', type=str, default='50,100,200,500,1000',
+                        help='different sizes split of the training set')
+
+    args = parser.parse_args()
+
+    input_file = args.input_train
+    input_dir = os.path.dirname(input_file)
+
+    data_size = map(int, args.data_size.split(','))
+
+    with open(input_file, 'r') as f:
+        lines = f.readlines()
+        lines = [x.strip() for x in lines]
+
+    data = []
+    example = []
+    for line in lines:
+        if line == '':
+            data.append(example)
+            example = []
+        else:
+            example.append(line)
+
+
+    np.random.seed(16)
+    np.random.shuffle(lines)
+
+    splits = []
+    for size in data_size:
+        splits.append(data[:size])
+        print(size)
+
+    for split in splits:
+        print(len(split))
+        with open(os.path.join(input_dir, 'train_' + str(len(split))), 'w') as f:
+            for example in split:
+                for line in example:
+                    f.write(line + '\n')
+                f.write('\n')
